904fcc3e244fa1441ffe654f6992f8041a731d1d
kyle tolie <kyle.tolie@1905newmedia.com>
2025-07-16 11:46:57 -0500

    linting errors

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth";
+// import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth"; // You already created this
 
 const handler = NextAuth(authOptions);
