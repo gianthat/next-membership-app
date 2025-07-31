21019eed9364bab82b9dd91440662e68e61dfeb1
kyle tolie <kyle.tolie@1905newmedia.com>
2025-07-31 09:28:28 -0500

    moving authoptions outside of route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,6 @@
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-
-export const authOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-  ],
-};
+import { authOptions } from "@/lib/authOptions";
 
 const handler = NextAuth(authOptions);
+
 export { handler as GET, handler as POST };
